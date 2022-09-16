d012f56888d15a3da244abde725546ced72c61d8
hoang4963 <hoangth.finbox@gmail.com>
2022-09-16 14:42:21 +0700

    a

--- truyvanquanlyhocvien.sql
+++ truyvanquanlyhocvien.sql
@@ -7,3 +7,7 @@ select count(student.id),address from student inner join address on student.addr
 select count(student.id),address from student inner join address on student.address_id = address.id and address.id = 5;
 select avg(thepoint),name from points inner join course on course.id = points.course_id and course.id = 2;
 select avg(thepoint),name from points inner join course on course.id = points.course_id and course.id = 1;
+select C.id, C.name, avg(thepoint)
+from course C join points P on c.id = P.course_id
+group by c.id, c.name
+having avg(thepoint) >= all (select avg(thepoint) from points group by points.course_id);
